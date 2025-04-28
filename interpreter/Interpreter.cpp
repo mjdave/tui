403fa1de6c91fef734bd9f41b1e2f06f71300e81
Dave Frampton <davidframpton@majicjungle.com>
2025-04-28 18:19:59 +1200

    linux building

--- interpreter/Interpreter.cpp
+++ interpreter/Interpreter.cpp
@@ -6,23 +6,23 @@ Interpreter::Interpreter(std::vector<std::string>& args)
     //TuiRef* debugRef = TuiRef::load(getResourcePath("examples/daveTest.tui"));
    // debugRef->debugLog();
     
-    /*for(auto& arg : args)
+    for(auto& arg : args)
     {
         TuiRef* scriptRunResult = TuiTable::runScriptFile(getResourcePath(arg));
         if(scriptRunResult)
         {
             scriptRunResult->debugLog();
         }
-    }*/
+    }
     
     
     //TuiRef* scriptRunResult = TuiTable::runScriptFile("examples/daveTest.tui");
     //TuiRef* scriptRunResult = TuiTable::runScriptFile("examples/scope.tui");
     //TuiRef* scriptRunResult = TuiTable::runScriptFile("examples/simpleScript.tui");
-    TuiRef* scriptRunResult = TuiTable::runScriptFile("examples/functionsAndIfStatements.tui");
+    //TuiRef* scriptRunResult = TuiTable::runScriptFile("examples/functionsAndIfStatements.tui");
     //TuiRef* scriptRunResult = TuiTable::runScriptFile("examples/mathsAndExpressions.tui");
-    if(scriptRunResult)
+   // if(scriptRunResult)
     {
-        scriptRunResult->debugLog();
+    //    scriptRunResult->debugLog();
     }
 }
