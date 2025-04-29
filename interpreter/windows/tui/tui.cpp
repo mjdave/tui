a85991226a5f447623fa584625f53416a5edab23
Dave Frampton <davidframpton@majicjungle.com>
2025-04-29 19:04:06 +1200

    windows solution

---
+++ interpreter/windows/tui/tui.cpp
@@ -0,0 +1,26 @@
+// tui.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Interpreter.h"
+#include "TuiLog.h"
+
+int main(int argc, const char* argv[])
+{
+    std::vector<std::string> args;
+    for(int i = 1; i < argc; i++)
+    {
+        args.push_back(argv[i]);
+    }
+
+    if(args.empty())
+    {
+        TuiLog("Please supply a script to run, eg. ./tui examples/example.tui");
+        return 0;
+    }
+
+    Interpreter* interpreter = new Interpreter(args);
+    delete interpreter;
+
+    return 0;
+}
