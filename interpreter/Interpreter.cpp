365c8e0ffc6fcb5a476e6c7049e512881cf31508
Dave Frampton <davidframpton@majicjungle.com>
2025-05-08 21:13:31 +1200

    fix linux build issues, and example script missing quotes

--- interpreter/Interpreter.cpp
+++ interpreter/Interpreter.cpp
@@ -7,24 +7,24 @@ Interpreter::Interpreter(std::vector<std::string>& args)
    // TuiRef* debugRef = TuiRef::runScriptFile("examples/scope.tui");
    // debugRef->debugLog();
     
-    /*for(auto& arg : args)
+    for(auto& arg : args)
     {
         TuiRef* scriptRunResult = TuiRef::runScriptFile(Tui::getResourcePath(arg));
         if(scriptRunResult)
         {
             scriptRunResult->debugLog();
         }
-    }*/
+    }
     
-    bool debugLogging = false;
-    TuiRef* scriptRunResult = TuiRef::runScriptFile("tests/tests.tui", debugLogging);
+    //bool debugLogging = false;
+    //TuiRef* scriptRunResult = TuiRef::runScriptFile("tests/tests.tui", debugLogging);
     //TuiRef* scriptRunResult = TuiRef::runScriptFile("examples/scope.tui");
     //TuiRef* scriptRunResult = TuiRef::runScriptFile("examples/functionExpressions.tui");
     //TuiRef* scriptRunResult = TuiRef::runScriptFile("examples/simpleScript.tui");
     //TuiRef* scriptRunResult = TuiRef::runScriptFile("examples/functionsAndIfStatements.tui");
     //TuiRef* scriptRunResult = TuiRef::runScriptFile("examples/mathsAndExpressions.tui");
-    if(scriptRunResult)
-    {
-        scriptRunResult->debugLog();
-    }
+    //if(scriptRunResult)
+    //{
+    //    scriptRunResult->debugLog();
+    //}
 }
--- interpreter/linux/build.sh
+++ interpreter/linux/build.sh
@@ -4,7 +4,9 @@ sleep 2
 if cmake --build build -- -j4; then
 mkdir -p tui
 cp -f bin/tui tui/tui
-ln -sf ../../../examples tui/examples
+ln -sf ../../../examples tui/
+ln -sf ../../../tests tui/
+echo "\nThe tui interpreter binary is now installed in ./tui/\nRun an example with:\n\ncd tui\n./tui examples/example.tui\n\n"
 else
 exit 1;
 fi
